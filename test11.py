71a1e89cea6dc1ab6de769b87bbadf5dd7da288e
akkarawat <akkkarwattanthlkul@gmail.com>
2026-09-02 09:25:36 +0700

    fix bug

---
+++ test11.py
@@ -0,0 +1,7 @@
+def main():
+    outfile = open('philosophers.txt','w')
+    outfile.write('john locke\n')
+    outfile.write('David hume\n')
+    outfile.write('edund burke\n')
+    outfile.close()
+main()
